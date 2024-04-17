ba7f42a476cd59b27c7e1d3389317c0b96b59461
Lucas Fonseca Alves <lukitasdaestatistica@gmail.com>
2024-04-16 21:41:55 -0300

    Análise 1, fiz um boxplot

---
+++ rdocs/Lucas Alves.R
@@ -0,0 +1,10 @@
+#------------------------Análise1---------------------------------
+
+dsadslalkdlças
+daksdlaksdsl
+ldkasjdlaj
+dsajdksaljdklajsajdaslasd
+
+#----------------------------Análise 2--------------
+djaslkdjalkdja
+
